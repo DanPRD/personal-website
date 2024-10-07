966f79ff69403493609f5bbfaa29cc71ede03162
Dan Pritchard <danjpritchard7@gmail.com>
2024-10-07 18:05:22 +0100

    added simple logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,55 @@
-use axum::{routing::get, Router};
+use std::time::Duration;
+
+use axum::{body::Bytes, extract::MatchedPath, http::{HeaderMap, Request}, response::Response, routing::get, Router};
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{info_span, Span};
+use tracing_subscriber::{layer::{self, SubscriberExt}, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async  {"the start"}));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:1111").await.unwrap();
+    tracing_subscriber::registry()
+    .with(
+        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            format!(
+                "{}=debug,tower_http=debug,axum::rejection=trace",
+                env!("CARGO_CRATE_NAME")
+            )
+            .into()
+        }),
+    )
+    .with(tracing_subscriber::fmt::layer())
+    .init();
 
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/biome", get(biome_generator))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request ",
+                        method = ?request.method(),
+                        matched_path,
+                    )
+                }));
+
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:1111").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
+async fn index() -> &'static str {
+    "meow meow"
+}
+
+
+async fn biome_generator() -> &'static str{
+    "this is a biome gen"
+}
